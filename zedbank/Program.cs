4ee84f90ab2e98fa2d84385615310f791e1cbf4e
Enyioma Osondu <o.enyioma@gmail.com>
2022-10-26 12:59:03 +0100

    readme and migrate on server startup

--- zedbank/Program.cs
+++ zedbank/Program.cs
@@ -85,6 +85,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<Context>();
+    context.Database.Migrate();
+}
+
 app.UseAuthentication();
 app.UseAuthorization();
 
